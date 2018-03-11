f2dc6b641ccbcf46752368c0c2094436c9990a11
Konstantin Manaenkov <manaenkov.konstantin@outlook.com>
2018-03-11 13:27:37 +0300

    update subjects model, style teachers block on main page

--- models/Subject.js
+++ models/Subject.js
@@ -4,12 +4,16 @@ var Types = keystone.Field.Types;
 var Subject = new keystone.List('Subject', {
 	map: { name: 'name' },
 	autokey: { path: 'slug', from: 'name', unique: true },
+	label: 'Дисциплины',
+	plural: 'Дисциплины',
+	singular: 'Дисциплина'
 });
 
 Subject.add({
-	name: { type: String, required: true },
-	description: { type: String },
+	name: { type: String, required: true, label: 'Название' },
+	description: { type: String, label: 'Описание' },
+	category: { type: Types.Select, options: 'Бакалавриат, Магистратура, Аспирантура, Доп. образование' , label: 'Категория'}
 });
 
-Subject.defaultColumns = 'name, position|20%';
+Subject.defaultColumns = 'name, category|20%';
 Subject.register();
